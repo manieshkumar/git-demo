1c5b234f186b085349b8364197ab82d82153fbc5
manieshkrsharma <itzmemaniesh.asd@gmail.com>
2020-02-03 19:14:10 +0530

    added git demo project to local repo

---
+++ src/main/java/com/git/testing/App.java
@@ -0,0 +1,12 @@
+package com.git.testing;
+
+public class App {
+
+	public void addData(String data) {
+		System.out.println("Data = "+data);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
