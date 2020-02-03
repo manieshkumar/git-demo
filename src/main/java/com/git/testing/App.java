17af71ead7077aad27f267be45562c1f1310855f
manieshkrsharma <itzmemaniesh.asd@gmail.com>
2020-02-03 19:16:56 +0530

    added if condition to check the data

--- src/main/java/com/git/testing/App.java
+++ src/main/java/com/git/testing/App.java
@@ -4,6 +4,9 @@ public class App {
 
 	public void addData(String data) {
 		System.out.println("Data = "+data);
+		if(data.equals("javaTechie")) {
+			System.out.println("Welcome");
+		}
 	}
 
 	public static void main(String[] args) {
